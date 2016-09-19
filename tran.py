f810acc66aad7a687d4a3dbe38fc48e2722558dd
kakitgogogo <707828130@qq.com>
2016-09-19 17:25:23 +0800

    Add files via upload

--- tran.py
+++ tran.py
@@ -13,26 +13,57 @@ elif len(sys.argv)==1:
 key="0EAE08A016D6688F64AB3EBB2337BFB0";
 url="http://dict-co.iciba.com/api/dictionary.php?w="+word+"&key="+key+"&type=json"
 
+init='\033[0m'
+black='\033[30m'
+red='\033[31m'
+green='\033[32m'
+yellow='\033[33m'
+blue='\033[34m'
+purple='\033[35m'
+
+def color_print(str,color=black):
+	print color+str+'\033[0m'
+
 try:
 	page=urllib2.urlopen(url)
 except Exception:
 	print "翻译失败"
 
 data=json.loads(page.read())
+'''
+output=json.dumps(data,indent=4)
+fd=open("data.json","w")
+fd.write(output)
+'''
+if sys.argv[1].isalpha():
+	try:
+		ph_en=data["symbols"][0]["ph_en"]
+		if ph_en:
+			color_print('['+ph_en+']',green)
+		ph_am=data["symbols"][0]["ph_am"]
+		if ph_am:
+			color_print('['+ph_am+']',green)
+		parts=data["symbols"][0]["parts"]
+		for item in parts:
+			print (yellow+"%-5s"+init)%item["part"],
+			string=str()
+			for mean in item["means"]:
+				string+=mean+'; '
+			color_print(string,blue)
+	except Exception:
+		color_print("翻译失败",red)
 
-def yellow_print(str):
-	print '\033[33m'+str+'\033[0m'
-
-def blue_print(str):
-	print '\033[34m'+str+'\033[0m'
-
-yellow_print("Means:")
-means=data["symbols"][0]["parts"][0]["means"]
-i=1
-for item in means:
-	blue_print(str(i)+". "+item["word_mean"])
-	i+=1;
-
-yellow_print("Pronunciation: ")
-pron=data["symbols"][0]["word_symbol"]
-blue_print(pron)
+else:
+	try:
+		means=data["symbols"][0]["parts"][0]["means"]
+		i=1
+		color_print("Results:",yellow)
+		for item in means:
+			color_print(str(i)+". "+item["word_mean"],blue)
+			i+=1
+		color_print("Pronunciation: ",yellow)
+		pron='['+data["symbols"][0]["word_symbol"]+']'
+		color_print(pron,blue)
+	except Exception:
+		color_print("翻译失败",red)
+		
