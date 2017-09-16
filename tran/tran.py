0b01cf1c2bce9eaf3f13f3fd780290313c6c4635
kakit <707828130@qq.com>
2017-09-16 10:13:45 +0800

    '更新为支持python3'

--- tran/tran.py
+++ tran/tran.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import json
-import sys
-import urllib2
+import sys, os
+import requests
 import getopt
+import platform
 
-key="0EAE08A016D6688F64AB3EBB2337BFB0";
+sysinfo = platform.system()
+
+key="0EAE08A016D6688F64AB3EBB2337BFB0"
 
 init='\033[0m'
 black='\033[30m'
@@ -16,7 +19,10 @@ blue='\033[34m'
 purple='\033[35m'
 
 def color_print(str,color=black):
-	print color+str+'\033[0m'
+	if sysinfo == 'Linux':
+		print(color+str+'\033[0m')
+	else:
+		print(str)
 
 def en2cn(data):
 	try:
@@ -28,13 +34,15 @@ def en2cn(data):
 			color_print('['+ph_am+']',green)
 		parts=data["symbols"][0]["parts"]
 		for item in parts:
-			print (yellow+"%-5s"+init)%item["part"],
+			color_print(("%-5s")%item["part"], yellow)
 			string=str()
 			for mean in item["means"]:
 				string+=mean+'; '
 			color_print(string,blue)
-	except Exception:
-		color_print("翻译失败",red)
+	except Exception as e:
+		color_print("翻译失败:"+str(e),red)
+		_, _, exc_tb = sys.exc_info()
+		print('lineno:', exc_tb.tb_lineno)
 
 def cn2en(data):
 	try:
@@ -47,27 +55,32 @@ def cn2en(data):
 		color_print("Pronunciation: ",yellow)
 		pron='['+data["symbols"][0]["word_symbol"]+']'
 		color_print(pron,blue)
-	except Exception:
-		color_print("翻译失败",red)
+	except Exception as e:
+		color_print("翻译失败:"+str(e),red)
+		_, _, exc_tb = sys.exc_info()
+		print('lineno:', exc_tb.tb_lineno)
 
 def main():
 	if len(sys.argv)>=2:
 		word=sys.argv[1]
 	elif len(sys.argv)==1:
-		word=raw_input("输入查询词：")
+		word=input("输入查询词：")
 	url="http://dict-co.iciba.com/api/dictionary.php?w="+word+"&key="+key+"&type=json"
 
 	try:
-		page=urllib2.urlopen(url)
-	except Exception:
-		color_print("翻译失败",red)
+		page=requests.get(url)
 
-	data=json.loads(page.read())
+		data=page.json()
 
-	if word.isalpha():
-		en2cn(data)
-	else:
-		cn2en(data)
+		if not('\u4e00' <= word[0] <= '\u9fa5'):
+			en2cn(data)
+		else:
+			cn2en(data)
+	except Exception as e:
+		color_print("翻译失败:"+str(e),red)
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		print(exc_type, fname, exc_tb.tb_lineno)
 		
 if __name__=="__main__":
 	main()
